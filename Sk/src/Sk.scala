8a7165d910628b0bebb29d5a011b9e3a6c19fc29
Дмитрий Гуляев <gulyaev4237@gmail.com>
2025-08-01 17:47:34 +0300

    пу

---
+++ Sk/src/Sk.scala
@@ -0,0 +1,29 @@
+package Sk
+
+import scalatags.Text.all.*
+import scalatags.Text.tags2.{section, title => ttitle, main => tmain}
+import cask.*
+
+object Sk extends cask.MainRoutes:
+  @cask.get("/s.css")
+  def s() = 
+    val path =
+      os.root / "home" / "dimany" / "th" / "all_src" / "mid" / "sk" / "Sk" / "src" / "s.css"
+    val content = os.read(path)
+    content
+  
+  @cask.get("/")
+  def h() =
+    html(lang:="ru")(
+      head(
+        meta(charset:="UTF-8"),
+        meta(httpEquiv:="X-UA-Compatible", content:="IE=edge"),
+        meta(name:="viewport", content:="width=device-width, initial-scale=1.0"),
+        ttitle("sk"),
+        link(rel:="stylesheet", href:="/s.css"),
+      ),
+      body(
+        
+      )
+    )
+  initialize()
